d9943c6a4004ee22b97978742016a19542d328c0
xrming <xrming@live.unc.edu>
2021-11-30 13:30:17 -0500

    undefined?

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -174,7 +174,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-    everyEven(arr,x => x%2 === 1);
+    everyEven(arr,x => x%2 == 1);
 };
 
 
@@ -184,7 +184,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-    someEven(arr,x => x%2 === 1);
+    someEven(arr, x=>x%2==1);
 };
 
 
