9d3d5266de4fdc4fc8f73c14b1675efc4c047e69
xrming <xrming@live.unc.edu>
2021-11-30 13:27:21 -0500

    try

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -161,8 +161,8 @@ export const filter = (arr, test) => {
     const result = new Object();
     result.pass= arr.filter(test);
     result.fail= arr.filter(donotinclude());
-    function donotinclude() {
-        return !result.pass.includes();
+    function donotinclude(x) {
+        return !result.pass.includes(x);
     }
     return result;
 };
@@ -174,7 +174,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    everyEven(arr,x => x%2 === 1);
 };
 
 
@@ -184,7 +184,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-    everyEven(arr,x => x%2 === 1);
+    someEven(arr,x => x%2 === 1);
 };
 
 
