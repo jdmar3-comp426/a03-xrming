1d9e0e53e6ed6c4f2cfda8c878b05093b53fa06b
xrming <xrming@live.unc.edu>
2021-12-01 23:10:03 -0500

    update const to let

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -102,7 +102,7 @@ export function removeKeyNonDestructive(object, key) {
 export function removeKeys(object, keyList) {
    const current = Object.assign({},object);
    for (let i=0; i<keyList.length; i++) {
-   const next = removeKeyNonDestructive(current,keyList[i]);
+   let next = removeKeyNonDestructive(current,keyList[i]);
    current = next;
    }
    return current;
